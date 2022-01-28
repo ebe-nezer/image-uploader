32c440b23ddfc6886590ff808640632126558d95
Ebenezer <ebenezerebi500@gmail..com>
2022-01-28 15:56:50 +0530

    bug fixed

--- controller/upload.ts
+++ controller/upload.ts
@@ -12,11 +12,10 @@ export const imageUpload = async (
   const { client, db } = await connectToDatabase();
   client.connect().then(() => {
     console.log("Connected to db");
-
+    const protocol =
+      process.env.NODE_ENV === "production" ? "https://" : "http://";
     const copyPath =
-      process.env.NODE_ENV === "production"
-        ? "https://"
-        : "http://" + req.headers.host + "/image/" + req.file.filename;
+      protocol + req.headers.host + "/image/" + req.file.filename;
     db.collection("uploads")
       .insertOne({ ...req.file, copyPath })
       .then((response) => {
