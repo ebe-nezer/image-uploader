b225ab5c13b64dd212f49c960a899b6b914f1588
Ebenezer <ebenezerebi500@gmail..com>
2022-01-28 15:35:16 +0530

    Image moved to cloudinary from gridfsbucket

--- controller/upload.ts
+++ controller/upload.ts
@@ -1,11 +1,31 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase } from "../lib/connectDB";
 
 interface ReqModifiedType extends NextApiRequest {
   [key: string]: any;
 }
+export const imageUpload = async (
+  req: ReqModifiedType,
+  res: NextApiResponse
+) => {
+  // const data = req.file;
+  const { client, db } = await connectToDatabase();
+  client.connect().then(() => {
+    console.log("Connected to db");
 
-export const imageUpload = (req: ReqModifiedType, res: NextApiResponse) => {
-  const file = req.file;
-  const others = req.body;
-  return res.status(200).json({ message: "Image Uploaded", file, ...others });
+    const copyPath =
+      process.env.NODE_ENV === "production"
+        ? "https://"
+        : "http://" + req.headers.host + "/image/" + req.file.filename;
+    db.collection("uploads")
+      .insertOne({ ...req.file, copyPath })
+      .then((response) => {
+        return res.status(200).json({
+          message: "uploaded",
+          ...response,
+          filename: req.file.filename,
+        });
+      })
+      .catch((err) => res.status(401).json({ message: "error", err }));
+  });
 };
